1ab564bff7f551233fe1a5849a1796215cadc981
navi1896 <ivanalejandroreyes1@gmail.com>
2026-07-13 17:19:18 -0600

    feat: Create SongSelect screen placeholder

---
+++ src/screens/SongSelect.gd
@@ -0,0 +1,22 @@
+extends Control
+
+class_name SongSelect
+
+@onready var scene_manager: SceneManager = get_tree().root.get_node("App/SceneManager")
+
+
+func _ready() -> void:
+	var label = Label.new()
+	label.text = "SongSelect - En desarrollo"
+	label.anchor_left = 0.5
+	label.anchor_top = 0.5
+	label.offset_left = -100
+	label.offset_top = -20
+	add_child(label)
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			scene_manager.load_scene("main_menu")
+			get_tree().root.set_input_as_handled()
